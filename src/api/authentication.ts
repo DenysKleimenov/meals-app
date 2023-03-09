d71b3e8af3088c1d62968d923ccdcb0a8674f119
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-03-09 18:54:28 +0200

    API requests were created

---
+++ src/api/authentication.ts
@@ -0,0 +1,15 @@
+/* eslint-disable arrow-body-style */
+import { User } from '../types/User';
+import { client } from '../utils/fetchClient';
+
+export const signUp = ({ username, email, password }: User) => {
+  return client.post<User>('/auth/signup', { username, email, password });
+};
+
+export const signIn = ({ email, password }: User) => {
+  return client.post<User>('/auth/signin', { email, password });
+};
+
+export const activate = (activationToken: string) => {
+  return client.get<User>(`/auth/activation/${activationToken}`);
+};
