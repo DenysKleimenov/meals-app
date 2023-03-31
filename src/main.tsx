d82ae4f071fd888901a2b086cccb627de6c03171
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-03-31 20:13:49 +0300

    Redux toolkit was added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+import { createTheme, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { App } from './App';
+import { store } from './app/store';
 import './index.css';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
