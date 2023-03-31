eb09c7b525045fea358ae275b074597beb08614f
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-03-31 20:07:43 +0300

    AuthForm component was created

--- src/types/Error.ts
+++ src/types/Error.ts
@@ -2,4 +2,6 @@ export enum Error {
   NONE,
   EMAIL = 'The email is in the wrong format or empty',
   PASSWORDS = 'Passwords are not identical',
+  EXISTS = 'User with this email already exists',
+  INCORRECT = 'Wrong email or password',
 }
